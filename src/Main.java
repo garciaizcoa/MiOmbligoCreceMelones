7c5df79237c0c868f757d5950cb3abf6effca96b
davidcarrionbeniquez <davidcarrionbeniquez@192.168.2.10>
2018-06-29 01:50:11 -0400

    Se comenzo a trabajar con el GUI.  Se añadio menu principal y  menu de inventario.  Se puede añadir un objeto al inventario usando el GUI.

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,41 @@
-
-public class Main {
-
-	public static void main(String[] args){
-		
-		Inventory inventory = new Inventory();
-		Menu menu = new Menu();
-		
-		inventory.addItemToInventory("buns", 5);
-		inventory.addItemToInventory("meat", 3);
-		inventory.addItemToInventory("bbq sauce", 2);
-		
-		inventory.addItemToInventory("lettuce", 1);
-		inventory.addItemToInventory("tomato", 1);
-		
-		Plate bbqBurger = new Plate("bbqBurger",5.99,  inventory);
-		bbqBurger.addIngredient("buns", 2);
-		bbqBurger.addIngredient("meat", 1);
-		bbqBurger.addIngredient("bbq sauce", 1);
-		
-		Plate salad = new Plate("salad", 2.99,inventory);
-		salad.addIngredient("lettuce", 1);
-		salad.addIngredient("tomato", 1);
-		
-		
-		menu.addPlate(bbqBurger);
-		menu.addPlate(salad);
-	
-		
-		Ticket ticket = new Ticket(menu, inventory);
-		ticket.placeOrder(1,bbqBurger);
-		ticket.placeOrder(1,bbqBurger);
-		ticket.placeOrder(1,bbqBurger);
-		ticket.placeOrder(1, salad);
-		//comment
-	
-		
-	}
-}
+//
+//public class Main {
+//
+//	public static void main(String[] args){
+//		
+////		Inventory inventory = new Inventory();
+////		Menu menu = new Menu();
+//		
+//		GUI gui = new GUI();
+//		
+//		gui.getInventory().addItemToInventory("buns", 5);
+//		gui.getInventory().addItemToInventory("meat", 3);
+//		gui.getInventory().addItemToInventory("bbq sauce", 2);
+//		
+//		gui.getInventory().addItemToInventory("lettuce", 1);
+//		gui.getInventory().addItemToInventory("tomato", 1);
+//		
+//		Plate bbqBurger = new Plate("bbqBurger",5.99,  gui.getInventory());
+//		bbqBurger.addIngredient("buns", 2);
+//		bbqBurger.addIngredient("meat", 1);
+//		bbqBurger.addIngredient("bbq sauce", 1);
+//		
+//		Plate salad = new Plate("salad", 2.99,gui.getInventory());
+//		salad.addIngredient("lettuce", 1);
+//		salad.addIngredient("tomato", 1);
+//		
+//		
+//		gui.getMenu().addPlate(bbqBurger);
+//		gui.getMenu().addPlate(salad);
+//	
+//		
+//		Ticket ticket = new Ticket(gui.getMenu(), gui.getInventory());
+//		ticket.placeOrder(1,bbqBurger);
+//		ticket.placeOrder(1,bbqBurger);
+//		ticket.placeOrder(1,bbqBurger);
+//		ticket.placeOrder(1, salad);
+//		//comment
+//	
+//		
+//	}
+//}
