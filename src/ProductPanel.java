bf42fb6cb7698f56732c84a88d3a34277ad99fb7
davidcarrionbeniquez <davidcarrionbeniquez@192.168.0.16>
2018-07-08 16:44:00 -0400

    Se añadieron los cambios de Christopher al customer menu

--- src/ProductPanel.java
+++ src/ProductPanel.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -19,28 +20,46 @@ public class ProductPanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private JLabel number;
+
 
 	public ProductPanel(Frame frame, Plate plate) {
 
 		
 				JButton productBtn = new JButton("");
-				productBtn.setIcon(new ImageIcon("C:\\Users\\chris\\Desktop\\testMeal.png"));
+				productBtn.setIcon(null);
 				
-				JButton menos = new JButton("");
+				JButton menos = new JButton("-");
+				menos.setFont(new Font("Tahoma", Font.PLAIN, 6));
 				menos.setForeground(Color.BLACK);
-				menos.setIcon(new ImageIcon("C:\\Users\\chris\\Desktop\\menos.png"));
+				menos.setIcon(null);
+				menos.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent menos) {
+						int cantidad = Integer.parseInt(number.getText());
+						if(cantidad != 0) {
+						--cantidad;
+						}
+						String answer = String.valueOf(cantidad);
+						number.setText(answer);
+					}
+				});
 				
-				JLabel number = new JLabel("0");
+				number = new JLabel("0");
 				number.setHorizontalAlignment(SwingConstants.CENTER);
 				number.setFont(new Font("Tahoma", Font.BOLD, 12));
 				
-				JButton mas = new JButton("");
+				JButton mas = new JButton("+");
+				mas.setFont(new Font("Tahoma", Font.PLAIN, 6));
 				mas.setForeground(Color.BLACK);
 				mas.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
+					public void actionPerformed(ActionEvent mas) {
+						int cantidad = Integer.parseInt(number.getText());
+						++cantidad;
+						String answer = String.valueOf(cantidad);
+						number.setText(answer);
 					}
 				});
-				mas.setIcon(new ImageIcon("C:\\Users\\chris\\Desktop\\mas.png"));
+				mas.setIcon(null);
 				GroupLayout gl_productPanel = new GroupLayout(this);
 				gl_productPanel.setHorizontalGroup(
 					gl_productPanel.createParallelGroup(Alignment.TRAILING)
@@ -75,5 +94,7 @@ public class ProductPanel extends JPanel {
 		
 		
 	}
+
+
 	
 }
